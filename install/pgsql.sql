4179e1b1b6a45a777515ee86dd4a972ef648e87e
Devin Smith <github@arzynik.com>
2015-11-30 15:36:15 -0800

    update query deploy scripts

--- install/pgsql.sql
+++ install/pgsql.sql
@@ -1,33 +1,7 @@
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
-CREATE OR REPLACE TYPE upload_type AS ENUM ('image', 'text');
-
-CREATE OR REPLACE FUNCTION upload_uid() RETURNS trigger AS $$
-	BEGIN
-		NEW.uid := REPLACE(CAST(gen_random_uuid() as char(36)),'-','');
-		RETURN NEW;
-	END;
-$$ LANGUAGE plpgsql;
-
-# leaving here for the future.
-#CREATE TABLE "upload" (
-#	"id" serial primary key,
-#	"date" timestamp DEFAULT current_timestamp,
-#	"type" upload_type,
-#	"uid" character varying DEFAULT NULL UNIQUE,
-#	"ext" varchar(20) DEFAULT NULL,
-#	"size" integer,
-#	"data" bytea DEFAULT NULL
-#);
-
-CREATE TABLE "upload" (
+CREATE TABLE "link" (
 	"id" serial primary key,
-	"date" timestamp DEFAULT current_timestamp,
-	"type" upload_type,
-	"uid" character varying DEFAULT NULL UNIQUE,
-	"ext" varchar(20) DEFAULT NULL,
-	"size" integer,
-	"data" text DEFAULT NULL
+	"url" varchar(255) DEFAULT NULL,
+	"hits" integer,
+	"permalink" character varying DEFAULT NULL UNIQUE,
+	"date" timestamp DEFAULT current_timestamp
 );
-
-CREATE TRIGGER "upload_uid" BEFORE INSERT ON "upload" FOR EACH ROW EXECUTE PROCEDURE upload_uid();
